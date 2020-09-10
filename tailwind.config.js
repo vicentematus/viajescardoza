334ba1eff755c4dd51e563245b948afcd83849f2
vicentematus <vicentematusortiz@gmail.com>
2020-09-10 18:53:36 -0300

    turismo cardoza

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("./colors");
+
+module.exports = {
+  purge: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+      },
+      height: {
+        viajePortada: "37.5rem",
+        halfScreen: "50vh",
+      },
+    },
+    colors,
+  },
+  variants: {},
+  plugins: [require("@tailwindcss/custom-forms")],
+};
