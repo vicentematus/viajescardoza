be535646406b18179db452b8f0cf96e27dd5a32d
jferreiro1997 <jferreiro1997@gmail.com>
2020-09-25 16:16:56 -0300

    Admin Front-end

--- src/app.js
+++ src/app.js
@@ -16,6 +16,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 //Base de Datos
 mongoose.connect('mongodb://localhost/ViajesCardoza', {useNewUrlParser: true, useUnifiedTopology: true});
 
