dd4608f2819ff09d06123fe63e8ab447953da4f4
vicentematus <vicentematusortiz@gmail.com>
2020-10-02 13:24:56 -0300

    cambio el puerto a 80 en app.js

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const paqueteRoutes = require('./routes/paquete');
 
 //App Config
 const app = express();
-const port = 5000;
+const port = 80;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
