6a0a7eb82f8288c6848e22b34d682698e8cf49b4
jferreiro1997 <jferreiro1997@gmail.com>
2020-10-03 22:19:03 -0300

    Update Static Method

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('src/public'));
 app.use(helmet());
 
 //Base de Datos
