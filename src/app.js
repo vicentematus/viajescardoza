96459fa334c14cb13634f6aa53139855f1b73696
jferreiro1997 <jferreiro1997@gmail.com>
2020-10-03 23:46:01 -0300

    Helmet FIX

--- src/app.js
+++ src/app.js
@@ -21,8 +21,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use('/public', express.static(path.join(__dirname, './public')));
 app.use(helmet());
+app.use('/public', express.static(path.join(__dirname, './public')));
 
 //Base de Datos
 mongoose.connect('mongodb://localhost/ViajesCardoza', {useNewUrlParser: true, useUnifiedTopology: true});
